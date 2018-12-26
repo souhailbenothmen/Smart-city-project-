9c2169ef3073e328e8d79682b1d591b25f0932e0
souhail ben othmen <souhail.benothmen@esprit.tn>
2018-12-26 12:24:33 +0100

    fix last add

--- back - Copie/models/PredictionSchema.js
+++
@@ -1,8 +0,0 @@
-var mongoose = require('mongoose');
-var valid=require('validator')
-var PredictionSchema= mongoose.Schema({
-    ARSON:{type:Number}
-    
-    });
-
-module.exports =mongoose.model('Prediction',PredictionSchema);
